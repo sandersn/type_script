55f3a2e953f7f6ff427043ec8342a773a75e241e
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-05-28 15:29:17 -0700

    Parse matched parentheses
    The grammar and data structure is now recursive!

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ struct Id {
     text: String,
 }
 enum Exp {
-    Number(i32)//,
-    //ParenExp(&Exp)
+    Number(i32),
+    Paren(Box<Exp>),
 }
 fn hello_parser(i: &str) -> nom::IResult<&str, &str> {
     nom::bytes::complete::tag("hello")(i)
@@ -13,19 +13,42 @@ fn hello_parser(i: &str) -> nom::IResult<&str, &str> {
 fn id(i: &str) -> nom::IResult<&str, Id> {
     nom::sequence::pair(
         nom::character::complete::alpha0,
-        nom::character::complete::alphanumeric0
-    )(i).map(|(rest, (id1,id2))| (rest, Id { text: format!("{}{}", id1, id2) }))
+        nom::character::complete::alphanumeric0,
+    )(i)
+    .map(|(rest, (id1, id2))| {
+        (
+            rest,
+            Id {
+                text: format!("{}{}", id1, id2),
+            },
+        )
+    })
+}
+fn expnum(i: &str) -> nom::IResult<&str, Exp> {
+    match nom::character::complete::digit1(i) {
+        Ok((rest, sn)) => match sn.parse::<i32>() {
+            Ok(i) => Ok((rest, Exp::Number(i))),
+            Err(_) => Err(nom::Err::Error((
+                "number of out range probably",
+                nom::error::ErrorKind::TooLarge,
+            ))),
+        },
+        Err(e) => Err(e),
+    }
+}
+fn expparen(i: &str) -> nom::IResult<&str, Exp> {
+    match nom::sequence::tuple((
+        nom::character::complete::char('('),
+        exp,
+        nom::character::complete::char(')'),
+    ))(i)
+    {
+        Ok((rest, (_, e, _))) => Ok((rest, Exp::Paren(Box::new(e)))),
+        Err(e) => Err(e),
+    }
 }
 fn exp(i: &str) -> nom::IResult<&str, Exp> {
-    //nom::error::ErrorKind
-    match nom::character::complete::digit1(i) {
-        Ok((rest,sn)) =>
-            match sn.parse::<i32>() {
-                Ok(i) => Ok((rest, Exp::Number(i))),
-                Err(_) => Err(nom::Err::Error(("number of out range probably", nom::error::ErrorKind::TooLarge)))
-            }
-        Err(e) => Err(e)
-    }
+    nom::branch::alt((expnum, expparen))(i)
 }
 fn main() {
     println!("{:?}", hello_parser("hello"));
@@ -35,10 +58,11 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use crate::exp;
     use crate::hello_parser;
     use crate::id;
-    use crate::exp;
     use crate::Exp::Number;
+    use crate::Exp::Paren;
     #[test]
     fn it_works() {
         assert_eq!(hello_parser("hello").unwrap(), ("", "hello"));
@@ -51,11 +75,24 @@ mod tests {
         assert_eq!(res.text, String::from("ab1m1"));
     }
     #[test]
-    fn exp_basic() {
+    fn exp_number() {
         let (rest, res) = exp("123").unwrap();
         assert_eq!(rest, "");
         match res {
             Number(n) => assert_eq!(n, 123),
+            Paren(_) => panic!("didn't expect to see a parenthesis here"),
+        }
+    }
+    #[test]
+    fn exp_paren() {
+        let (rest, res) = exp("(123)").unwrap();
+        assert_eq!(rest, "");
+        match res {
+            Number(_) => panic!("Didn't expect a number here"),
+            Paren(e) => match *e {
+                Number(n) => assert_eq!(n, 123),
+                Paren(_) => panic!("Didn't expect a paren here"),
+            },
         }
     }
 }
